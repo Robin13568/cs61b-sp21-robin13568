10aab866ecc7067895508917f3c54e6b36e8562e
Robin13568 <rzhou13568@gmail.com>
2022-11-06 18:10:12 -0500

    done with Lab 3: Timing Tests and Randomized Comparison Tests

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,54 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    public static void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> a = new AListNoResizing<>();
+        BuggyAList<Integer> b = new BuggyAList<>();
+
+        a.addLast(4);
+        a.addLast(5);
+        a.addLast(6);
+
+        b.addLast(4);
+        b.addLast(5);
+        b.addLast(6);
+
+        assertEquals(a.size(), b.size());
+
+        assertEquals(a.removeLast(), b.removeLast());
+        assertEquals(a.removeLast(), b.removeLast());
+        assertEquals(a.removeLast(), b.removeLast());
+    }
+
+    public static void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> Lb = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                Lb.addLast(randVal);
+//                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+//                System.out.println("size: " + L.size());
+                assertEquals(L.size(), Lb.size());
+            } else if (L.size() > 0 && operationNumber == 2) {
+                // getLast
+                assertEquals(L.getLast(), Lb.getLast());
+            } else if (L.size() > 0 && operationNumber == 3) {
+                // removeLast
+                assertEquals(L.removeLast(), Lb.removeLast());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        testThreeAddThreeRemove();
+        randomizedTest();
+    }
 }
--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -12,7 +12,7 @@ public class TimeAList {
             int N = Ns.get(i);
             double time = times.get(i);
             int opCount = opCounts.get(i);
-            double timePerOp = time / opCount * 1e6;
+            double timePerOp = time / opCount * 1e6; // The fourth column microsec/op gives the number of microseconds it took on average to complete each call to addLast.
             System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
         }
     }
@@ -23,5 +23,24 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>(); // The first column N gives the size of the data structure (how many elements it contains).
+        AList<Double> times = new AList<>(); // The second column time (s) gives the time required to complete all operations.
+        AList<Integer> opCounts = new AList<>(); // The third column # ops gives the number of calls to addLast made during the timing experiment.
+
+        int N = 500;
+        for (int i = 1; i <= 8; i++) {
+            N = N * 2;
+            Ns.addLast(N);
+
+            AList<Integer> al = new AList<>();
+            Stopwatch sw = new Stopwatch();
+            for (int j = 1; j <= N; j++) {
+                al.addLast(1);
+            }
+            times.addLast(sw.elapsedTime());
+            opCounts.addLast(N);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 }
